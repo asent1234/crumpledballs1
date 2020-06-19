bb4d4945d0731f6f2ac306d87590ea2d7c8a4120
asent1234 <42815464+asent1234@users.noreply.github.com>
2020-06-18 19:37:20 -0700

    Update paper.js

--- paper.js
+++ paper.js
@@ -6,13 +6,14 @@ class Paper {
             'density':1.0,
             'isStatic' : false
         }
-        this.body = Bodies.circle(x, y, radius, options, 0);
+        this.body = Bodies.circle(x, y, radius, options);
 	    World.add(world, this.body);
         this.radius = radius
         
    }
    display(){
     //fill(34,34,34)
+    ellipseMode(RADIUS);
     ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius) 
     //ellipse(56, 46, 55, 55)  
    }
